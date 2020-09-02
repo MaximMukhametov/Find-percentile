67cb462b6c8f43834f9fb36699bcb3b049387028
MaximMukhametov <viashino92@mail.ru>
2020-09-02 22:49:16 +0700

    edit numpy solution

--- percentile_numpy.py
+++ percentile_numpy.py
@@ -1,12 +1,11 @@
 from numpy import percentile, array
 from functools import partial
 
+# create a function to get the twentieth percentile
+twentieth_percentile = partial(percentile, q=20)
 
 if __name__ == "__main__":
     # convert the list of numeric strings to numpy.array of integers
     value_list = array(input().split(), dtype=int)
-
-    # create a function to get the twentieth percentile
-    twentieth_percentile = partial(percentile, q=20)
 
     print(twentieth_percentile(value_list))
